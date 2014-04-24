0fddb4c8e9bebaf3a31c8e470ebe941c5357e455
nodecode.de <themedropbox@nodecode.de>
2014-04-25 01:12:35 +0200

    New theme: ColorLight - 1.0
    git-svn-id: http://themes.svn.wordpress.org/colorlight@31845 8c925393-aaf8-4f5c-984e-1f1965effe94

---
+++ footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage ColorLight
+ * @since ColorLight 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">Theme <a href="http://wordpress.org/themes/colorlight">ColorLight</a> by <a href="http://nodecode.de/" title="Node.js Tutorial">NodeCode</a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php
+add_theme_support( 'custom-background', array(
+	'default-color' => '81d742',
+) );
