97b5e2cf21e78c56b35b76472a06aa8ce2bb45ed
J. Daniel Schmidt <mail@jdsn.de>
2014-04-25 01:15:37 +0200

    fork as colorlight-frederking

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
-			<?php printf( __( 'Theme %s by %s', 'colorlight' ), '<a href="http://wordpress.org/themes/colorlight" target="_blank">ColorLight</a>', 'NodeCode' ); ?>
+			<?php printf( __( 'Theme based on %s by %s', 'colorlight' ), '<a href="http://wordpress.org/themes/colorlight" target="_blank">ColorLight</a>', 'NodeCode' ); ?> | Powered by <a href="http://wordpress.org">Wordpress</a> | Hosted by <a href="http://schmidtcom.de">schmidtcom.de</a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php
 add_theme_support( 'custom-background', array(
-	'default-color' => '81d742',
+	'default-color' => 'E6E6E6',
 ) );
+
+function mytheme_dequeue_fonts() {
+    wp_dequeue_style( 'twentytwelve-fonts' );
+}
+add_action( 'wp_enqueue_scripts', 'mytheme_dequeue_fonts', 11 );
+
+?>
